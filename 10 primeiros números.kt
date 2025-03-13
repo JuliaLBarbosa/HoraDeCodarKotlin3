8b8a666349f9d627ef332c38a86af59e1cb686ad
JuliaLBarbosa <juliamargarida2244@gmail.com>
2025-03-13 03:00:18 -0300

    Add files via upload

---
+++ 10 primeiros números.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    var contador = 101
+
+    println("Digite os 10 primeiros números maiores que 100")
+
+    while(contador > 100 && contador <= 111){
+        println(contador)
+        contador += 1
+    }
+
+}
